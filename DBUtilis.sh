5ff85332644d98563a7e9ac00997e57ee999f8cd
shalinivepanjeri <shalinivepanjeri123@gmail.com>
2023-04-11 07:31:28 +0530

    merge conflict line in development branch

--- DBUtilis.sh
+++ DBUtilis.sh
@@ -5,3 +5,5 @@ This is the first line in local repository, master branch.
 This is the second line in local repository, master branch.
 
 This is the third line in local repository, master branch.
+
+adding merge conflict line in development branch.
