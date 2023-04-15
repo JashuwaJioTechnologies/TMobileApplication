b91286f169ab63c506327845495b41a2d135274b
shalinivepanjeri <130329810+shalinivepanjeri@users.noreply.github.com>
2023-04-15 13:02:12 +0530

    Update instanceCreation.tf

--- terraformscripts/instanceCreation.tf
+++ terraformscripts/instanceCreation.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+    region = "ap-south-1"
+}
 
+resource "aws_instance" "AWSINSTANCE" {
+    count = "2"
+    ami = "ami-0fdea1353c525c182"
+    instance_type = "t2.micro"
+    key_name = "jashuwavepanjerirajendran"
+    security_groups = ["launch-wizard-2"]
+    tags = {
+        Name = "appServers"
+    }
+}
